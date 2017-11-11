e68afbe4c8667e11543edfe3ffe9cbd4cc576660
Ainor Syahrizal <macintorch@gmail.com>
2017-11-12 03:32:24 +0800

    invoking cloud function

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,17 +1,41 @@
 import React, {Component} from 'react';
 import {View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+//to make network request
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-one-time-password-2f210.cloudfunctions.net';
 
 class SignUpForm extends Component {
+
+	//defining state
+	state = { phone: ''};
+
+	//ES2017
+	// async await system 
+	handleSubmit = () => {
+		axios.post(`${ROOT_URL}/createUser`, {
+			phone: this.state.phone
+		})
+			.then( () => {
+				axios.post(`${ROOT_URL}/requestOneTimePassword`, {
+					phone: this.state.phone
+				})
+
+			}) 
+	}
 
 	render(){
 		return(
 			<View>
 				<View style={{ marginBottom: 10 }}>	
 					<FormLabel>Enter Phone Number </FormLabel>
-					<FormInput/>
-					<Button title="Submit" />
+					<FormInput
+					value={this.state.phone}
+					onChangeText={phone => this.setState({ phone })}/>
 				</View>
+					<Button title="Submit" 
+					onPress={this.handleSubmit}/>
 			</View>
 			);
 	}
